f582768373ff5eab806bccaf58369d3bc0894630
moxiawujiang <32701668+moxiawujiang@users.noreply.github.com>
2022-05-25 14:55:55 +0800

    Update login_case.py

--- test_case/login_case.py
+++ test_case/login_case.py
@@ -33,14 +33,14 @@ class LoginTest(unittest.TestCase):
         LoginPage(self.driver).check_exist_failmsg()
 
     # #登录测试
-    # @data(("13709048313@163.com","605332571qwe"))
+    # @data(("13709048313@163.com","123456"))
     # @unpack
     # @get_screen_in_case_end_or_error
     # def test_login001(self,username,password):
     #     self._testMethodDoc='''用户登录成功'''
     #     LoginPage(self.driver).login(username,password)
     #     #校验登录后的用户名
-    #     HomePage(self.driver).check_username('张孝雪')
+    #     HomePage(self.driver).check_username('张小雪')
 
 
     # @get_screen_in_case_end_or_error
